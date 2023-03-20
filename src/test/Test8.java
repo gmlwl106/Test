cb31ce8a6c3057dd7f39a3919fc2ddd12785dcfa
gmlwl106 <66778894@naver.com>
2023-03-20 14:09:07 +0900

    대문자와 소문자

---
+++ src/test/Test8.java
@@ -0,0 +1,25 @@
+package test;
+
+/* 대문자와 소문자
+문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+public class Test8 {
+	public static String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++) {
+        	if(Character.isUpperCase(my_string.charAt(i))) {
+        		answer += Character.toLowerCase(my_string.charAt(i));
+        	} else {
+        		answer += Character.toUpperCase(my_string.charAt(i));
+        	}
+        }
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		String my_string = "cccCCC";
+		System.out.println(solution(my_string));
+	}
+	
+}
