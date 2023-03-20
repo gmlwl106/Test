cdd5d8071e09e252794ed5ccd53802bd2106f951
gmlwl106 <66778894@naver.com>
2023-03-20 10:09:42 +0900

    자릿수 더하기

---
+++ src/test/Test3.java
@@ -0,0 +1,22 @@
+package test;
+
+/* 자릿수 더하기
+정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+ */
+
+public class Test3 {
+	public static int solution(int n) {
+        int answer = 0;
+        String[] number = String.valueOf(n).split("");
+        for(String num : number) {
+        	answer += Integer.parseInt(num);
+        }
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		int n = 1234;
+		System.out.println(solution(n));
+	}
+	
+}
