051a60d2688e22a45e54031ff68401116448dc3c
gmlwl106 <66778894@naver.com>
2023-03-20 09:40:37 +0900

    삼각형의 완성조건(1)

---
+++ src/test/Test1.java
@@ -0,0 +1,34 @@
+package test;
+
+/* 삼각형의 완성조건 (1)
+선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
+ */
+
+public class Test1 {
+	public static int solution(int[] sides) {
+        int answer = 0;
+        int max = 0;
+        int minSum = 0;
+        
+        for(int side: sides) {
+        	if(side > max) {
+        		minSum += max;
+        		max = side;
+        	} else {
+        		minSum += side;
+        	}
+        }
+
+        if(max < minSum) answer = 1; else answer = 2;
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		int[] sides = {3, 6, 2};
+		System.out.println(solution(sides));
+	}
+	
+}
