8eb1e3526b119e609ff68348b5d4bdd35f7ff37e
gmlwl106 <66778894@naver.com>
2023-03-20 11:09:39 +0900

    제곱수 판별하기

---
+++ src/test/Test5.java
@@ -0,0 +1,18 @@
+package test;
+
+/* 제곱수 판별하기
+어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+ */
+
+public class Test5 {
+	public static int solution(int n) {
+		//Math.sqrt : 제곱수 판별 함수
+        return Math.sqrt(n) % 1 == 0 ? 1 : 2;
+    }
+
+	public static void main(String[] args) {
+		int n = 144;
+		System.out.println(solution(n));
+	}
+	
+}
